130f9fcfd1e79d4ccbf66c19e488790628a1218d
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2014-02-26 20:48:52 +0100

    Fixing DSSComputer

--- practical-work/DGtalSkel/DSSExample.cpp
+++ practical-work/DGtalSkel/DSSExample.cpp
@@ -4,7 +4,7 @@
 #include <DGtal/base/Common.h>
 #include <DGtal/io/boards/Board2D.h>
 #include <DGtal/helpers/StdDefs.h>
-#include <DGtal/geometry/curves/ArithmeticalDSS.h>
+#include <DGtal/geometry/curves/ArithmeticalDSSComputer.h>
 ///////////////////////////////////////////////////////////////////////////////
 
 using namespace std;
@@ -15,17 +15,17 @@ using namespace DGtal::Z2i; //We'll only consider Z² digital space on
 
 int main()
 {
-  
+
   Board2D board;
   Domain domain(Point(0,0),Point(8,4));
-  
+
   //We construct a sequence of points
   std::vector<Point> sequence;
   sequence.push_back(Point(0,0));
   sequence.push_back(Point(1,0));
   sequence.push_back(Point(2,1));
   sequence.push_back(Point(3,1));
-  
+
   board << domain;
 
   //We check the sequence
@@ -35,19 +35,19 @@ int main()
 	itend = sequence.end(); it != itend ; ++it)
     board << (*it); //We trace out the sequence
   board.saveEPS("sequence.eps");
-  
+
   //a 8-connected arithmetical DSS
   // to define a 4-connected one, change the last template parameter
-  ArithmeticalDSS< Iterator, Integer, 8> a8DSS;
+  ArithmeticalDSSComputer< Iterator, Integer, 8> a8DSS;
 
   //We branch the DSS to the point sequence
   a8DSS.init( sequence.begin() );
-  
+
   //We display the parameters
   std::cout << a8DSS <<std::endl;
 
   //We do one step and check if the next point was in the DSS
-  if ( a8DSS.extendForward() )
+  if ( a8DSS.extendFront() )
     {
       std::cout << "All fine.. DSS = "<< a8DSS;
     }
@@ -57,8 +57,8 @@ int main()
     }
 
   //We process all points in the sequence
-  while ( a8DSS.extendForward() ) ;
-  
+  while ( a8DSS.extendFront() ) ;
+
   //we display the DSS
   std::cout << "Final parameters DSS="<<a8DSS<<std::endl;
 
@@ -67,16 +67,16 @@ int main()
   board << domain ;
   //We draw the DSS points
   board << SetMode(Point().className(), "Grid")
-	<< SetMode(a8DSS.className(), "Points")
-	<< a8DSS ;
+    	<< SetMode(a8DSS.primitive().className(), "Points")
+	<< a8DSS.primitive() ;
   //..and the DSS bounding box
   board << SetMode(Point().className(), "Grid")
-	<< SetMode(a8DSS.className(), "BoundingBox")
-	<< a8DSS ;
-  
+    	<< SetMode(a8DSS.primitive().className(), "BoundingBox")
+	<< a8DSS.primitive() ;
+
   // export in SVG this time..
   board.saveSVG("DSS.svg");
- 
+
 
   return 0;
 }
