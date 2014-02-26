04fd59b861b986e9b855392c0295cc197072092b
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2014-02-26 20:51:40 +0100

    edit TD5

--- practical-work/DGtalSkel/DSSCirculatorExample.cpp
+++ practical-work/DGtalSkel/DSSCirculatorExample.cpp
@@ -4,7 +4,7 @@
 #include <DGtal/base/Common.h>
 #include <DGtal/io/boards/Board2D.h>
 #include <DGtal/helpers/StdDefs.h>
-#include <DGtal/geometry/curves/ArithmeticalDSS.h>
+#include <DGtal/geometry/curves/ArithmeticalDSSComputer.h>
 #include <DGtal/base/Circulator.h>
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -16,10 +16,10 @@ using namespace DGtal::Z2i; //We'll only consider Z² digital space on
 
 int main()
 {
-  
+
   Board2D board;
   Domain domain(Point(-1,-1),Point(8,4));
-  
+
   //We construct a sequence of points
   std::vector<Point> sequence;
   sequence.push_back(Point(0,0));
@@ -30,7 +30,7 @@ int main()
   sequence.push_back(Point(0,3));
   sequence.push_back(Point(-1,2));
   sequence.push_back(Point(-1,1));
-  
+
   board << domain;
 
   //We check the sequence
@@ -40,7 +40,7 @@ int main()
 	itend = sequence.end(); it != itend ; ++it)
     board << (*it); //We trace out the sequence
   board.saveEPS("sequenceCirculator.eps");
-  
+
   //Circulator on the sequence
   typedef Circulator<Iterator> MyCirculator;
 
@@ -53,27 +53,27 @@ int main()
   //extremities.
   //For example, let's display the triplet of consecutive points
   MyCirculator itbegin = it;
-  
+
   do
     {
       MyCirculator next = it, nextnext= it ;
       next++;
       nextnext ++; nextnext ++;
-      trace.info() << "current = "<< (*it) 
-		   << " next = "<< (*next) 
+      trace.info() << "current = "<< (*it)
+		   << " next = "<< (*next)
 		   << " next-next = " << (*nextnext)<<std::endl;
 
       ++it;
     }
   while (it != itbegin);
-  
+
   //We can now define a DSS on the Circulator to be able to recognize
   //DSS on circular contours (e.g. there may exist a DSS covering
-  //sequence extremities). 
-  ArithmeticalDSS< MyCirculator, Integer, 8> a8DSS;
-  
+  //sequence extremities).
+  ArithmeticalDSSComputer< MyCirculator, Integer, 8> a8DSS;
+
   //API on aDSS remains valid (extend, retract, ...)
   //...
- 
+
   return 0;
 }
